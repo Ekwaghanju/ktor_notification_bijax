b0b07aa4ed423f66e8fc4d381302d7e428c9d6fe
Ekwaghanju <116279836+Ekwaghanju@users.noreply.github.com>
2025-01-06 00:53:52 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+
+group = "com.theodore"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+
+    implementation("com.google.firebase:firebase-admin:9.4.2")
+}
+
+
+tasks.register<Jar>("fatJar") {
+    group = "build"
+    description = "Assembles a fat JAR including dependencies"
+    archiveClassifier.set("all")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes["Main-Class"] = "io.ktor.server.netty.EngineMain" // Main class
+    }
+
+    from(sourceSets.main.get().output)
+
+    // Include all dependencies
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
+
